b24bf278cd1c2ad413c3347255bdc1ab563ef546
fatih-58 <fatihfurkanturan@gmail.com>
2025-10-29 13:13:57 +0100

    gegner haben mehr gesundheit. Es gibt 2 verschiedene gegener

--- Assets/scripts/EnemyAttack.cs
+++ Assets/scripts/EnemyAttack.cs
@@ -2,21 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAttack : MonoBehaviour {
+public class EnemyAttack : MonoBehaviour
+{
+    public int health = 2; // Düşmanın canı - 2 mermiye kadar dayanacak
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player")
+        if (other.CompareTag("Player"))
         {
-            Destroy(other.gameObject);
+            Destroy(other.gameObject); // Oyuncuya temas ederse, oyuncuyu yok et
         }
-        else if(other.tag == "Bullet")
+        else if (other.CompareTag("Bullet"))
         {
-            Destroy(other.gameObject);
-            Destroy(gameObject);
-        }
+            Destroy(other.gameObject); // Mermiyi yok et
 
+            // Düşmanın canını azalt
+            health--;
+
+            // Canı sıfır veya altına düştüyse düşmanı yok et
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
-
 }
-
